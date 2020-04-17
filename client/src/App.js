9354ef4ba6664290b8a5fb9e2e9d145eee0108d4
waddah-jd <waddah.ja@gmail.com>
2020-04-17 19:56:35 +0300

    add react and config docker to use it

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <header>
+        <h1>url-shortener</h1>
+      </header>
+    </div>
+  );
+};
+
+export default App;
